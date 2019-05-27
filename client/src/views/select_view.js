ed66e2f9e39149e8b8de03a81f76235d179b9959
marisol <mtoscanor37@gmail.com>
2019-05-27 09:11:26 +0100

    Added homework

---
+++ client/src/views/select_view.js
@@ -0,0 +1,37 @@
+// const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
+
+// const SelectView = function (element) {
+//   this.element = element;
+// };
+
+export default class SelectView {
+  constractor(element) {
+    this.element = element;
+  }
+
+  bindEvents() {
+    PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+      const allInstrumentFamilies = evt.detail;
+      this.populate(allInstrumentFamilies);
+    });
+
+    this.element.addEventListener('change', (evt) => {
+      const selectedIndex = evt.target.value;
+      PubSub.publish('SelectView:change', selectedIndex);
+    });
+  };
+
+  populate(instrumentFamilyData) {
+    instrumentFamilyData.forEach((familiy, index) => {
+      const option = document.createElement('option');
+      option.textContent = familiy.name;
+      option.value = index;
+      this.element.appendChild(option);
+    });
+  };
+
+};
+
+// module.exports = SelectView;
+// export default SelectView;
